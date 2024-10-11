4fdce3d31cc0818f186b3f74d79545da4dba8471
cmedeiros1130 <cmedeiros1130@gmail.com>
2024-10-11 14:11:34 -0400

    updated front page

---
+++ src/components/Rank.js
@@ -0,0 +1,13 @@
+import "./Rank.css";
+
+function Rank() {
+  return (
+    <div className="rank-container">
+      <div className="rank">Chris, your current rank is...</div>
+      <br />
+      <div className="rank1">#5</div>
+    </div>
+  );
+}
+
+export default Rank;
---
+++ src/components/navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+      <p className="sign-out-nav"> Sign out</p>
+    </nav>
+  );
+};
+
+export default Navigation;
